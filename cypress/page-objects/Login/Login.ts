ac1a7688f2e7f9ebe76c6282cfe1783eb48ff2a8
tzahita <tamamtzahi@gmail.com>
2020-11-05 18:31:48 +0200

    server

--- cypress/page-objects/Login/Login.ts
+++ cypress/page-objects/Login/Login.ts
@@ -10,12 +10,15 @@ export class Login {
   private login_url: string = Cypress.env('LOGIN_URL');
 
   login() {
+    cy.server();
+    cy.route('POST', '**api/cards/all').as('all');
     cy.visit(this.login_url);
     cy.contains('Get Started').click();
     cy.url().should('include', '/getStarted');
     cy.get(selectors.emailInput).type(this.email);
     cy.get(selectors.passwordInput).type(this.password);
     cy.get(selectors.loginBtn).contains('Submit').click({ force: true });
+    cy.wait('@all').its('status').should('eq', 200)
     cy.url().should('include', '/feed');
   }
 }
